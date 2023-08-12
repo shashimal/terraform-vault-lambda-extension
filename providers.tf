c8daa12a09f2514d66628366ca2ea9f3fe272c3c
Duleendra <shashimald@gmail.com>
2023-08-12 23:47:06 +0800

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
-provider "vault" {
-  address = "http://13.229.130.210:8200/"
-  token   = ""
-}
+#provider "vault" {
+ # address = "http://13.229.130.210:8200/"
+  #token   = ""
+#}
 
 provider "aws" {
   region = "ap-southeast-1"
